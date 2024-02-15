044dcba718b4073ab5d0abd786ee68c25f1467dd
pietra alberganti <125596829+bpietra@users.noreply.github.com>
2024-02-15 09:23:49 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import {View} from 'react-native';
+
+import SimpleWidget from './SimpleWidget';
+
+
+
+const App = () => {
+
+return ( 
+
+  <View>
+
+<SimpleWidget/>
+
+<SimpleWidget/>
+
+<SimpleWidget/>
+
+  
+
+  </View>
+
+   );
+
+};
+
+
+
+ export default App;
+
+ 
