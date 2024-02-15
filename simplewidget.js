718bb90360946d75ba30bda9ca155de806b0485c
pietra alberganti <125596829+bpietra@users.noreply.github.com>
2024-02-15 09:24:31 -0300

    Create simplewidget.js

---
+++ simplewidget.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import {View, Text} from 'react-native';
+
+
+
+const SimpleWidget = () => {
+
+return (
+
+    <View>
+
+<Text> Meu primeiro widget </Text>
+
+    </View>
+
+);
+
+};
+
+export default SimpleWidget;
